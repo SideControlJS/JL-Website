0bca9bef3dbfbbe40571a13d99daf05bdab5afc3
Jesse <jesse.lare@gmail.com>
2024-04-20 19:57:16 -0400

    using Express urlencoded instead of bodyparser

--- server.js
+++ server.js
@@ -1,15 +1,11 @@
 require('dotenv').config();
 const express = require('express');
-const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(express.urlencoded({ extended: true }));
 app.use('/assets', express.static('/assets'));
-
-
 
 app.post('/sendmail', (req, res) => {
   if (!req.body.name || !req.body.email || !req.body.message) {
@@ -22,32 +18,32 @@ app.post('/sendmail', (req, res) => {
   }
 
   const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-          user: process.env.GMAIL_USER, 
-          pass: process.env.GMAIL_PASS
-        }
-      });
+    service: 'gmail',
+    auth: {
+      user: process.env.GMAIL_USER, 
+      pass: process.env.GMAIL_PASS
+    }
+  });
       
-      const mailOptions = {
-        from: req.body.email, // sender address
-        to: process.env.GMAIL_USER, // email from environment variable
-        subject: `New message from ${req.body.name}`, // Subject line
-        html: `<h1>New Message From ${req.body.name}</h1><p>${req.body.message}</p>` // HTML body
-    };
+  const mailOptions = {
+    from: req.body.email,
+    to: process.env.GMAIL_USER,
+    subject: `New message from ${req.body.name}`,
+    html: `<h1>New Message From ${req.body.name}</h1><p>${req.body.message}</p>`
+  };
       
-      transporter.sendMail(mailOptions, function (err, info) {
-        if (err) {
-          console.error(err);
-          res.status(500).send('Error sending email');
-        } else {
-          res.status(200).send('Email successfully sent');
-        }
-      });
-      
+  transporter.sendMail(mailOptions, function (err, info) {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Error sending email');
+    } else {
+      res.status(200).send('Email successfully sent');
+    }
+  });
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
+
